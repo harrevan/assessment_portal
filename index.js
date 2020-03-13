a02755b540742b93f627f97fa31aec237728a154
Evan Harrison <har16143@byui.edu>
2020-03-13 10:50:09 -0600

    json

--- index.js
+++ index.js
@@ -7,19 +7,7 @@ app.use(express.static("public"));
 app.set("views","views");
 app.set("view engine", "ejs");
 
-app.get("/calculate", function(req,res){
-    var mail_type = req.query.mail;
-    var weight = req.query.wt;
-    console.log(mail_type);
-    console.log(weight);
-    var rate = calculateRate(mail_type, weight);
 
-    var params = {mail: mail_type, weight: weight, rate: rate};
-    //var rounded = rate.toFixed(2);
-    //console.log(rounded);
-
-	res.render("results", params);
-});
 
 app.listen(PORT, function(){
 	console.log('Listening on port 5000');
