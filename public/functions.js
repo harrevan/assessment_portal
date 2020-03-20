f4ad249e3dadeeb7cd410b46a83f9cf30239869c
Evan Harrison <har16143@byui.edu>
2020-03-20 15:26:57 -0600

    moving js

--- public/functions.js
+++ public/functions.js
@@ -1,43 +1,66 @@
  function displayStudents(time) {
-  	  	var classTime = time;
-  	  	console.log(classTime);
-  	  	var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            var studentName = JSON.parse(this.responseText);
-            console.log(this.responseText);
-            console.log(studentName[0].upper);
+    var classTime = time;
+    console.log(classTime);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        var studentName = JSON.parse(this.responseText);
+        console.log(this.responseText);
+        console.log(studentName[0].upper);
 
-            createList(classTime, studentName);
-            }
-        };
-        xhttp.open("GET", "/students?time=" + classTime, true);
-        xhttp.send();
-      }
+        createList(classTime, studentName);
+        }
+    };
+    xhttp.open("GET", "/students?time=" + classTime, true);
+    xhttp.send();
+}
 
 function createList(time, studentName){
-	// Clear any existing content first
-	document.getElementById("student_list").innerHTML = "";
-	var title = document.createElement("H1");
-	title.innerHTML = time + " Students";
-	document.getElementById("student_list").appendChild(title);
+    	// Clear any existing content first
+    document.getElementById("student_list").innerHTML = "";
+  	var title = document.createElement("H1");
+  	title.innerHTML = time + " Students";
+  	document.getElementById("student_list").appendChild(title);
 
-	//Create list of students
-/*      	var but = document.createElement("BUTTON");
-	but.setAttribute("id", "students");
-	but.className = "list-group-item list-group-item-action";
-	document.getElementById("student_list").appendChild(ul);
-	for(var i  = 0; i < studentName.length; i++){
-		var li = document.createElement("LI");
-		li.innerHTML = studentName[i].upper;
-		console.log(studentName[i].upper);
-		document.getElementById("students").appendChild(li);
-	}*/
-	for(var i  = 0; i < studentName.length; i++){
-		var but = document.createElement("BUTTON");
-		but.setAttribute("id", "student" + i);
-		but.className = "list-group-item list-group-item-action";
-		but.innerHTML = studentName[i].upper;
-		document.getElementById("student_list").appendChild(but);
+  	//Create list of students
+  /*      	var but = document.createElement("BUTTON");
+  	but.setAttribute("id", "students");
+  	but.className = "list-group-item list-group-item-action";
+  	document.getElementById("student_list").appendChild(ul);
+  	for(var i  = 0; i < studentName.length; i++){
+  		var li = document.createElement("LI");
+  		li.innerHTML = studentName[i].upper;
+  		console.log(studentName[i].upper);
+  		document.getElementById("students").appendChild(li);
+  	}*/
+  	for(var i  = 0; i < studentName.length; i++){
+  		var but = document.createElement("BUTTON");
+  		but.setAttribute("id", "student" + i);
+  		but.className = "list-group-item list-group-item-action";
+  		but.innerHTML = studentName[i].upper;
+  		document.getElementById("student_list").appendChild(but);
 	}
+function getAssessmentUnit(unit){
+    console.log(unit);
+
 }
+
+function getAssessmentType(type){
+    console.log(type);
+}
+
+
+function displayAssessmentSelectOptions(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        var studentName = JSON.parse(this.responseText);
+        console.log(this.responseText);
+        console.log(studentName[0].upper);
+
+        createList(classTime, studentName);
+        }
+    };
+    xhttp.open("GET", "/assess_select_options", true);
+    xhttp.send();
+}  
