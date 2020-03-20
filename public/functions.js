e0614092751ccc37fbc4ad8a97f09d67b5f7371d
Evan Harrison <har16143@byui.edu>
2020-03-20 14:48:35 -0600

    moving js

--- public/functions.js
+++ public/functions.js
@@ -13,32 +13,31 @@
         };
         xhttp.open("GET", "/students?time=" + classTime, true);
         xhttp.send();
-
       }
 
-      function createList(time, studentName){
-      	// Clear any existing content first
-      	document.getElementById("student_list").innerHTML = "";
-      	var title = document.createElement("H1");
-      	title.innerHTML = time + " Students";
-      	document.getElementById("student_list").appendChild(title);
+function createList(time, studentName){
+	// Clear any existing content first
+	document.getElementById("student_list").innerHTML = "";
+	var title = document.createElement("H1");
+	title.innerHTML = time + " Students";
+	document.getElementById("student_list").appendChild(title);
 
-      	//Create list of students
+	//Create list of students
 /*      	var but = document.createElement("BUTTON");
-      	but.setAttribute("id", "students");
-      	but.className = "list-group-item list-group-item-action";
-      	document.getElementById("student_list").appendChild(ul);
-      	for(var i  = 0; i < studentName.length; i++){
-      		var li = document.createElement("LI");
-      		li.innerHTML = studentName[i].upper;
-      		console.log(studentName[i].upper);
-      		document.getElementById("students").appendChild(li);
-      	}*/
-      	for(var i  = 0; i < studentName.length; i++){
-      		var but = document.createElement("BUTTON");
-      		but.setAttribute("id", "student" + i);
-      		but.className = "list-group-item list-group-item-action";
-      		but.innerHTML = studentName[i].upper;
-      		document.getElementById("student_list").appendChild(but);
-      	}
-      }
+	but.setAttribute("id", "students");
+	but.className = "list-group-item list-group-item-action";
+	document.getElementById("student_list").appendChild(ul);
+	for(var i  = 0; i < studentName.length; i++){
+		var li = document.createElement("LI");
+		li.innerHTML = studentName[i].upper;
+		console.log(studentName[i].upper);
+		document.getElementById("students").appendChild(li);
+	}*/
+	for(var i  = 0; i < studentName.length; i++){
+		var but = document.createElement("BUTTON");
+		but.setAttribute("id", "student" + i);
+		but.className = "list-group-item list-group-item-action";
+		but.innerHTML = studentName[i].upper;
+		document.getElementById("student_list").appendChild(but);
+	}
+}
